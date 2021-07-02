6c825a2ae64204bc7de6cdd976a196b58bbe0b81
innosatyam <85342175+innosatyam@users.noreply.github.com>
2021-07-02 15:21:16 +0530

    WIP

--- gatsby-theme-mds/gatsby-node.js
+++ gatsby-theme-mds/gatsby-node.js
@@ -69,6 +69,7 @@ exports.onCreatePage = (
         titleType,
         frontmatter,
         MdxNode,
+        base: 'default'
       },
     });
   }
--- gatsby-theme-mds/src/util/NavItems.js
+++ gatsby-theme-mds/src/util/NavItems.js
@@ -2,26 +2,27 @@ import { useStaticQuery, graphql } from 'gatsby';
 
 export function useNavItems() {
   const {
-    allNavItemsYaml: { edges },
+    allNavYaml: { nodes },
   } = useStaticQuery(graphql`
-    query LEFT_NAV_QUERY {
-      allNavItemsYaml {
-        edges {
-          node {
-            label
-            link
-            subMenu {
+    query LEFT_NAV_QUERY($base: [String]) {
+      allNavYaml(filter: {base: {in: $base}}) {
+          nodes {
+            
+            menus {
               label
               link
+              subMenu {
+                label
+                link
+              }
             }
-            
           }
         }
-      }
     }
   `);
 
-  const navItems = edges.map(({ node }) => {
+
+  const navItems = nodes[0].menus.map((node) => {
 
     const menu = { ...node, name: node.label, icon: 'arrow_right_alt' };
 
